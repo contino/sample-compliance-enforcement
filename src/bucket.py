6b57da494dc70ee0cc120a2fb527ad8224cd681a
Emre Erkunt <emre.erkunt@gmail.com>
2019-02-06 11:02:40 +0000

    Fixed Bucket.is_private() where it was returning false results. Also added return to the methods

--- src/bucket.py
+++ src/bucket.py
@@ -18,12 +18,14 @@ class Bucket(object):
         self.messages.append(message)
         logger.info(message)
         self.results.append(result)
+        return result
 
     def is_encrypted(self):
         logger.info('Checking if {} S3 Bucket is encrypted.'.format(self.bucket_name))
         try:
             self.s3.get_bucket_encryption(Bucket=self.bucket_name)
             self._return(True, 'Bucket Encryption for {}: Enabled'.format(self.bucket_name))
+            return True
         except ClientError as e:
             if str(e).endswith('The server side encryption configuration was not found'):
                 self._return(False, 'Bucket Encryption for {}: Disabled'.format(self.bucket_name))
@@ -31,8 +33,10 @@ class Bucket(object):
                 if self.auto_fix:
                     self.set_encrypted()
 
+                return False
             else:
                 print('Exception: {}'.format(e))
+                return False
 
     def set_encrypted(self):
         logger.info('Changed bucket encryption for {} to ENABLED. [NOT IMPLEMENTED YET]')
@@ -40,14 +44,24 @@ class Bucket(object):
     def is_private(self):
         logger.info('Checking if {} S3 Bucket is private.'.format(self.bucket_name))
 
-        grantee = self.s3.get_bucket_acl(Bucket=self.bucket_name)['Grants'][0]['Grantee']
-        if 'URI' in grantee:
+        private = False
+        grantee = self.s3.get_bucket_acl(Bucket=self.bucket_name)['Grants']
+
+        for grant in grantee:
+            if 'URI' in grant['Grantee']:
+                private = True
+                print('Private Grantee: {}'.format(private))
+
+        if private:
             self._return(False, 'Bucket Access for {}: Public'.format(self.bucket_name))
+            return False
 
         self._return(True, 'Bucket Access for {}: Private'.format(self.bucket_name))
 
         if self.auto_fix:
             self.set_private()
+
+        return True
 
     def set_private(self):
         logger.info('Changed bucket access for {} to PRIVATE. [NOT IMPLEMENTED YET]')
